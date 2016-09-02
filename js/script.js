3c29278d1f63413c4484e905412d900f491712e8
Eddy Valencia <eddyvalencia8@gmail.com>
2016-09-02 11:54:36 -0430

    2do commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
+var band = true;
 $(function(){
+	
     $('#world-map').vectorMap({
       	map: 've_mill',
-      
-      	onRegionClick: function (event, code) {
-    	var regionName = map.getRegionName(code);
-		}
+      	onRegionClick: function(event, code) {
+        	
+    	},
+    	onRegionSelected: function(event, code, isSelected, selectedRegions) {
+    		if(isSelected){
+    			console.log(code);
+    			band = false;
+    		} 	
+    	},
+    	regionsSelectable: band,
+    	regionStyle:{
+    		selected:{
+    			fill:'red'
+    		}
+    	}
   	});
 });
