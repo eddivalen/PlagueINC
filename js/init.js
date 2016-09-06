c110ccc67471b391bfdf4588285f8e8d89f80951
Eddy Valencia <eddyvalencia8@gmail.com>
2016-09-06 16:02:41 -0430

    Se modificaron varias cosas
    Se modificó, el index.html, se agregó una variable code que son los
    codigos que le pertenecen a cada estado, se añadió la leyenda en el
    mapa.
    
    Se añadieron las funciones: colorInicial(), infectarEstado(),
    changeColor(), getColor(), getCodebyRegion(), activarBotones()

--- js/init.js
+++ js/init.js
@@ -2,34 +2,34 @@ var virus;
 if (typeof(global.info) === 'undefined') {
     global.info = {};
     global.info.estados = [
-    {nombre:'Zulia',poblacion:4323467,contagiados:0,muertes:0, adyacentes:['Mérida', 'Táchira', 'Trujillo', 'Falcón', 'Lara'], infectado:0},
-    {nombre:'Miranda',poblacion:3992374,contagiados:0,muertes:0, adyacentes:['Guárico', 'Anzoátegui', 'Aragua', 'Distrito Capital', 'Vargas'], infectado:0},
-    {nombre:'Carabobo',poblacion:3315506,contagiados:0,muertes:0, adyacentes: ['Cojedes', 'Guárico', 'Aragua', 'Yaracuy'], infectado:0},
-    {nombre:'Distrito Capital',poblacion:3137710,contagiados:0,muertes:0, adyacentes: ['Miranda', 'Aragua', 'Vargas'], infectado:0},
-    {nombre:'Lara',poblacion:2219211,contagiados:0,muertes:0, adyacentes: ['Falcón', 'Zulia', 'Trujillo', 'Portuguesa', 'Cojedes', 'Yaracuy'], infectado:0},
-    {nombre:'Aragua',poblacion:1976470,contagiados:0,muertes:0, adyacentes: ['Vargas', 'Carabobo', 'Cojedes', 'Guárico', 'Anzoátegui'], infectado:0},
-    {nombre:'Bolívar',poblacion:1874190,contagiados:0,muertes:0, adyacentes:['Amazonas','Apure','Anzoátegui','Delta Amacuro'], infectado:0},
-    {nombre:'Anzoátegui',poblacion:1788329,contagiados:0,muertes:0, adyacentes:['Monagas', 'Sucre', 'Miranda', 'Guárico', 'Bolívar', 'Nueva Esparta'], infectado:0},
-    {nombre:'Táchira',poblacion:1578108,contagiados:0,muertes:0,adyacentes:['Zulia','Mérida','Barinas','Apure'], infectado:0},
-    {nombre:'Sucre',poblacion:1071017,contagiados:0,muertes:0,adyacentes:['Monagas', 'Anzoátegui', 'Nueva Esparta'], infectado:0},
-    {nombre:'Falcón',poblacion:1029638,contagiados:0,muertes:0,adyacentes:['Zulia', 'Lara', 'Yaracuy'], infectado:0},
-    {nombre:'Portuguesa',poblacion:1012781,contagiados:0,muertes:0, adyacentes:['Barinas','Trujillo','Lara','Cojedes'], infectado:0},
-    {nombre:'Monagas',poblacion:998024,contagiados:0,muertes:0,adyacentes:['Delta Amacuro', 'Anzoátegui', 'Sucre', 'Bolívar'], infectado:0},
-    {nombre:'Mérida',poblacion:992971,contagiados:0,muertes:0,adyacentes:['Zulia', 'Táchira', 'Barinas','Trujillo'], infectado:0},
-    {nombre:'Barinas',poblacion:989439,contagiados:0,muertes:0,adyacentes:['Apure','Táchira', 'Mérida', 'Trujillo', 'Portuguesa', 'Guárico', 'Cojedes'], infectado:0},
-    {nombre:'Guárico',poblacion:870951,contagiados:0,muertes:0,adyacentes:['Anzoátegui', 'Bolívar', 'Apure','Barinas','Cojedes','Carabobo','Aragua', 'Miranda'], infectado:0},
-    {nombre:'Trujillo',poblacion:787988,contagiados:0,muertes:0, adyacentes:['Zulia','Mérida','Barinas','Portuguesa','Lara'], infectado:0},
-    {nombre:'Yaracuy',poblacion:693876,contagiados:0,muertes:0, adyacentes:['Falcón','Lara','Portuguesa','Cojedes', 'Carabobo'], infectado:0},
-    {nombre:'Apure',poblacion:587056,contagiados:0,muertes:0, adyacentes:['Bolívar','Guárico','Barinas','Táchira'], infectado:0},
-    {nombre:'Vargas',poblacion:398018,contagiados:0,muertes:0, adyacentes:['Miranda', 'Aragua', 'Distrito Capital'], infectado:0},
-    {nombre:'Cojedes',poblacion:348022,contagiados:0,muertes:0, adyacentes:['Carabobo', 'Yaracuy', 'Cojedes','Guárico','Aragua'], infectado:0},
-    {nombre:'Delta Amacuro',poblacion:187022,contagiados:0,muertes:0,adyacentes:['Bolívar','Monagas'], infectado:0},
-    {nombre:'Amazonas',poblacion:178670,contagiados:0,muertes:0, adyacentes:['Bolívar'], infectado:0},
-    {nombre:'Nueva Esparta',poblacion:552011,contagiados:0,muertes:0,adyacentes:['Sucre', 'Anzoátegui'],infectado:0},
+    {nombre:'Zulia',code:'VE-V',poblacion:4323467,contagiados:0,muertes:0, adyacentes:['Mérida', 'Táchira', 'Trujillo', 'Falcón', 'Lara'], infectado:0},
+    {nombre:'Miranda',code:'VE-M',poblacion:3992374,contagiados:0,muertes:0, adyacentes:['Guárico', 'Anzoátegui', 'Aragua', 'Distrito Capital', 'Vargas'], infectado:0},
+    {nombre:'Carabobo',code:'VE-G',poblacion:3315506,contagiados:0,muertes:0, adyacentes: ['Cojedes', 'Guárico', 'Aragua', 'Yaracuy'], infectado:0},
+    {nombre:'Distrito Capital',code:'VE-A',poblacion:3137710,contagiados:0,muertes:0, adyacentes: ['Miranda', 'Aragua', 'Vargas'], infectado:0},
+    {nombre:'Lara',code:'VE-K',poblacion:2219211,contagiados:0,muertes:0, adyacentes: ['Falcón', 'Zulia', 'Trujillo', 'Portuguesa', 'Cojedes', 'Yaracuy'], infectado:0},
+    {nombre:'Aragua',code:'VE-D',poblacion:1976470,contagiados:0,muertes:0, adyacentes: ['Vargas', 'Carabobo', 'Cojedes', 'Guárico', 'Anzoátegui'], infectado:0},
+    {nombre:'Bolívar',code:'VE-F',poblacion:1874190,contagiados:0,muertes:0, adyacentes:['Amazonas','Apure','Anzoátegui','Delta Amacuro'], infectado:0},
+    {nombre:'Anzoátegui',code:'VE-B',poblacion:1788329,contagiados:0,muertes:0, adyacentes:['Monagas', 'Sucre', 'Miranda', 'Guárico', 'Bolívar', 'Nueva Esparta'], infectado:0},
+    {nombre:'Táchira',code:'VE-S',poblacion:1578108,contagiados:0,muertes:0,adyacentes:['Zulia','Mérida','Barinas','Apure'], infectado:0},
+    {nombre:'Sucre',code:'VE-R',poblacion:1071017,contagiados:0,muertes:0,adyacentes:['Monagas', 'Anzoátegui', 'Nueva Esparta'], infectado:0},
+    {nombre:'Falcón',code:'VE-I',poblacion:1029638,contagiados:0,muertes:0,adyacentes:['Zulia', 'Lara', 'Yaracuy'], infectado:0},
+    {nombre:'Portuguesa',code:'VE-P',poblacion:1012781,contagiados:0,muertes:0, adyacentes:['Barinas','Trujillo','Lara','Cojedes'], infectado:0},
+    {nombre:'Monagas',code:'VE-N',poblacion:998024,contagiados:0,muertes:0,adyacentes:['Delta Amacuro', 'Anzoátegui', 'Sucre', 'Bolívar'], infectado:0},
+    {nombre:'Mérida',code:'VE-L',poblacion:992971,contagiados:0,muertes:0,adyacentes:['Zulia', 'Táchira', 'Barinas','Trujillo'], infectado:0},
+    {nombre:'Barinas',code:'VE-E',poblacion:989439,contagiados:0,muertes:0,adyacentes:['Apure','Táchira', 'Mérida', 'Trujillo', 'Portuguesa', 'Guárico', 'Cojedes'], infectado:0},
+    {nombre:'Guárico',code:'VE-J',poblacion:870951,contagiados:0,muertes:0,adyacentes:['Anzoátegui', 'Bolívar', 'Apure','Barinas','Cojedes','Carabobo','Aragua', 'Miranda'], infectado:0},
+    {nombre:'Trujillo',code:'VE-T',poblacion:787988,contagiados:0,muertes:0, adyacentes:['Zulia','Mérida','Barinas','Portuguesa','Lara'], infectado:0},
+    {nombre:'Yaracuy',code:'VE-U',poblacion:693876,contagiados:0,muertes:0, adyacentes:['Falcón','Lara','Portuguesa','Cojedes', 'Carabobo'], infectado:0},
+    {nombre:'Apure',code:'VE-C',poblacion:587056,contagiados:0,muertes:0, adyacentes:['Bolívar','Guárico','Barinas','Táchira'], infectado:0},
+    {nombre:'Vargas',code:'VE-X',poblacion:398018,contagiados:0,muertes:0, adyacentes:['Miranda', 'Aragua', 'Distrito Capital'], infectado:0},
+    {nombre:'Cojedes',code:'VE-H',poblacion:348022,contagiados:0,muertes:0, adyacentes:['Carabobo', 'Yaracuy', 'Cojedes','Guárico','Aragua'], infectado:0},
+    {nombre:'Delta Amacuro',code:'VE-Y',poblacion:187022,contagiados:0,muertes:0,adyacentes:['Bolívar','Monagas'], infectado:0},
+    {nombre:'Amazonas',code:'VE-Z',poblacion:178670,contagiados:0,muertes:0, adyacentes:['Bolívar'], infectado:0},
+    {nombre:'Nueva Esparta',code:'VE-O',poblacion:552011,contagiados:0,muertes:0,adyacentes:['Sucre', 'Anzoátegui'],infectado:0},
 
  
     ];
-     
+    global.info.puntos=0;
 }
 $('#jugar').on('click',function( ev ){
     ev.preventDefault();
@@ -41,5 +41,18 @@ $('#jugar').on('click',function( ev ){
         global.info.virus = virus;
         console.log(global.info.virus);
         window.location.href = "../html/game.html";
+        window.resizeTo(1015,800);
+        window.moveTo(150,150);
     }
 });
+
+function get_poblacioninicial(){
+    var poblacioninicial=0;
+
+        for (var i = 0; i < global.info.estados.length; i++) {
+             poblacioninicial+=global.info.estados[i].poblacion;
+        }
+    return poblacioninicial;
+}
+
+global.info.poblacioninicial=get_poblacioninicial();
--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 var band = true;
 var template = tabletemplate.innerHTML;
 table.innerHTML = _.template(template,{estados:global.info.estados});
+var map;
 
 $(function(){
-    var map,
 	map = new jvm.Map({
         container: $('#world-map'),
         map: 've_mill',
@@ -16,16 +16,171 @@ $(function(){
     	onRegionSelected: function(event, code, isSelected, selectedRegions) {
     		if(isSelected){
                 global.origen = map.getRegionName(code);
+                infectarEstado(code,'pink');
+               map.clearSelectedRegions();
                 console.log(global.origen);
                 band=false;
+
+                for (var i = 0; i < global.info.estados.length; i++) {
+                    if(global.origen == global.info.estados[i].nombre){
+                        global.info.estados[i].contagiados=global.info.estados[i].contagiados+1;
+                        global.info.estados[i].poblacion=global.info.estados[i].poblacion-1;
+                    }
+                }
     		} 	
     	},
+         series: {
+            markers:[{
+                 attribute: 'fill',
+                scale: {
+                    '30%': 'pink',
+                    '50%': 'red'
+                },
+                legend: {
+                  vertical: true
+               }
+            }],
+            regions: [{
+                attribute: 'fill',
+
+            }]
+        },
         regionsSelectable: true,
         regionsSelectableOne: true,
     	regionStyle:{
     		selected:{
-    			fill:'red'
+    			fill:'pink'
     		}
     	}
     });
 });
+var colorInicial = function(key){
+             var colors = {};
+            colors[key] = 'pink';
+             return colors;
+        };
+var infectarEstado = function(code){
+    obj = colorInicial(code);
+    map.series.regions[0].setValues(obj);
+}
+var changeColor = function(code,color){
+    var obj = {};
+    obj[code]=color;
+    map.series.regions[0].setValues(obj);
+}
+var getColor = function(code){
+    obj=colorInicial(code);
+var map = $('#world-map').vectorMap('get', 'mapObject');
+var color = map.series.regions[0].scale.getValue(code);
+console.log(color);
+}
+var getCodebyRegion = function(region){
+    var find = _.where(global.info.estados,{nombre:region});
+    return find[0].code;
+}
+var activarBoton_rojo = function(){
+    $('#bubble_red').removeClass('disabled');
+    $('#bubble_red').on('click',function( ev ){
+        ev.preventDefault();
+        value= 3;
+        global.info.puntos+= value;
+        document.getElementById("adn").innerHTML = global.info.puntos;
+         $('#bubble_red').addClass('disabled');
+    });
+}
+var activarBoton_naranja = function(){
+      $('#bubble_orange').removeClass('disabled');
+    $('#bubble_orange').on('click',function( ev ){
+        ev.preventDefault();
+        value= 3;
+        global.info.puntos+= value;
+        document.getElementById("adn").innerHTML = global.info.puntos;
+         $('#bubble_orange').addClass('disabled');
+    });
+}
+var activarBoton_azul = function(){
+    $('#bubble_blue').removeClass('disabled');
+     $('#bubble_blue').on('click',function( ev ){
+        ev.preventDefault();
+        //Retrasar tasa de cura
+    });
+}
+
+function get_poblaciontotal(){
+
+    var poblaciontotal=0;
+
+ for (var i = 0; i < global.info.estados.length; i++) {
+             poblaciontotal+=global.info.estados[i].poblacion;
+         }
+
+    return poblaciontotal;
+}
+
+function get_contagiadostotal(){
+
+    var contagiadostotal=0;
+
+ for (var i = 0; i < global.info.estados.length; i++) {
+             contagiadostotal+=global.info.estados[i].contagiados;
+         }
+
+    return contagiadostotal;
+}
+
+function get_muertestotal(){
+
+    var muertestotal=0;
+
+ for (var i = 0; i < global.info.estados.length; i++) {
+             muertestotal+=global.info.estados[i].muertes;
+         }
+
+    return muertestotal;
+}
+
+global.info.poblaciontotal=get_poblaciontotal();
+global.info.contagiadostotal=get_contagiadostotal();
+global.info.muertestotal=get_muertestotal();
+
+pbtotal.innerHTML = global.info.poblaciontotal;
+cttotal.innerHTML = global.info.contagiadostotal;
+mttotal.innerHTML = global.info.muertestotal;
+
+function get_porc_poblacion(){
+
+    var porc_poblacion=0;
+
+    porc_poblacion=(global.info.poblaciontotal/global.info.poblacioninicial)*100;
+
+    return porc_poblacion;
+}
+
+function get_porc_contagiados(){
+
+    var porc_contagiados=0;
+
+    porc_contagiados=(global.info.contagiadostotal/global.info.poblacioninicial)*100;
+
+    return porc_contagiados;
+}
+
+function get_porc_muertes(){
+
+    var porc_muertes=0;
+
+    porc_muertes=(global.info.muertestotal/global.info.poblacioninicial)*100;
+
+    return porc_muertes;
+}
+
+global.info.porc_poblacion=get_porc_poblacion().toFixed();
+global.info.porc_contagiados=get_porc_contagiados().toFixed();
+global.info.porc_muertes=get_porc_muertes().toFixed();
+
+document.getElementById("pbporcentaje").style.cssText = "width: "+global.info.porc_poblacion+"%;";
+document.getElementById("ctporcentaje").style.cssText = "width: "+global.info.porc_contagiados+"%;";
+document.getElementById("mtporcentaje").style.cssText = "width: "+global.info.porc_muertes+"%;";
+
+//$('#bubble_orange').removeClass('disabled');
+//$('#bubble_orange').addClass('disabled');
